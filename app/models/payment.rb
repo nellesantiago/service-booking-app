27fe0fa7df418e75675a2086bb2142177cb51431
Maia <maia@Maias-MacBook-Air.local>
2023-04-05 10:03:01 +0800

    cancel button

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,5 +3,5 @@ class Payment < ApplicationRecord
   belongs_to :booking
 
   enum payment_type: {cash: 0, card: 1}
-  enum payment_status: {unpaid: 0, paid: 1}
+  enum payment_status: {unpaid: 0, paid: 1, refunded: 2}
 end
